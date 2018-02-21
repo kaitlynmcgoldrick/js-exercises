236a6eb4ee143aa0d234ec20cb93d73799fd4144
rosexw <rose.wan@evermight.com>
2018-02-21 13:34:31 -0500

    promise exercise 1 works

--- promises/exercise-1.js
+++ promises/exercise-1.js
@@ -1,12 +1,23 @@
-require("es6-promise");
+// require("es6-promise");
 
 // Create a promise. Have it fulfilled with a value of 'FULFILLED!' in executor after a delay of 300ms, using setTimeout.
 // Then, print the contents of the promise after it has been fulfilled by passing console.log to then.
+//
+// const promise = new Promise(function(resolve, reject) {
+//   // Your solution here
+//   // This is what was done with Hilary Hacksel in class, but keeps giving errors...Error: Cannot find module 'es6-promise'
+//   setTimeout(() => {
+//     resolve('FULFILLED!');
+//   }, 300);
+// });
+//
+// const onResolve = value => {
+//   console.log(data);
+// };
+//
+// Promise.then(onResolve);
 
-const promise = new Promise(function(resolve, reject) {
-  // Your solution here
-});
 
-const onResolve = value => {
-  // Your solution here
-};
+const wait = ms => new Promise(resolve => setTimeout(resolve, 300));
+
+wait().then(() => Promise.resolve().then(() => console.log('FULFILLED!')));
