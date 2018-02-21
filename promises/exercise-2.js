b0687a62a3525a8afa7d28a868f8b2dd0dabc7ed
rosexw <rose.wan@evermight.com>
2018-02-21 15:48:18 -0500

    fixed exercise 2 s an error object and npm install to require the es promises

--- promises/exercise-2.js
+++ promises/exercise-2.js
@@ -1,19 +1,19 @@
-// require("es6-promise");
+require("es6-promise");
 
 // Create a promise that after a delay of 300ms, rejects with an Error object. The Error object should be constructed with parameter 'REJECTED!', which is the textual message of the error.
 // Create a function onReject to print error.message using console.log. Pass this function as a rejection handler to the then method of your promise.
 
 const promise = new Promise(function(resolve, reject) {
   // Your solution here
-    setTimeout(() => reject('REJECTED!'), 300);
+    setTimeout(() => reject(Error('REJECTED!')), 300);
   });
 
 const onReject = value => {
   // Your solution here
-  console.error(promise);
+  console.log(value.message);
 };
 
-promise.then(onReject);
+promise.then(err => onReject(err));
 
 
 //(node:23714) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 1): REJECTED!
