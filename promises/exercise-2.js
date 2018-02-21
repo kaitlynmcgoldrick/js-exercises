7007774b31edde366e3ecdb8ecd9cb3793c08fe4
rosexw <rose.wan@evermight.com>
2018-02-21 14:17:13 -0500

    commands work with promises returning fulfilled and rejected after 300ms.

--- promises/exercise-2.js
+++ promises/exercise-2.js
@@ -1,12 +1,18 @@
-require("es6-promise");
+// require("es6-promise");
 
 // Create a promise that after a delay of 300ms, rejects with an Error object. The Error object should be constructed with parameter 'REJECTED!', which is the textual message of the error.
 // Create a function onReject to print error.message using console.log. Pass this function as a rejection handler to the then method of your promise.
 
 const promise = new Promise(function(resolve, reject) {
   // Your solution here
-});
+    setTimeout(() => {
+      reject('REJECTED!');
+    }, 300);
+  });
 
 const onReject = value => {
   // Your solution here
+  console.log(promise);
 };
+
+promise.then(onReject);
