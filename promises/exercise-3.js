2fe03dd9eacaf29dd8a1bba3ca7e37ad004b81f8
rosexw <rose.wan@evermight.com>
2018-02-22 17:27:35 -0500

    exercise 3 works with fetching https://css-tricks.com/using-fetch/

--- promises/exercise-3.js
+++ promises/exercise-3.js
@@ -1,3 +1,14 @@
 const fetch = require('node-fetch');
 
 // Make a fetch request to https://next.json-generator.com/api/json/get/EJPkuFBIV
+
+const url = 'https://next.json-generator.com/api/json/get/EJPkuFBIV';
+
+fetch(url, {
+  method: 'get'
+})
+  .then(response => response.json())
+  .then(data => {
+      console.log(data)
+    })
+  .catch(error => console.log("The sky is falling", error));
